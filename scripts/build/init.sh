441875a39af623d9601c63936b13ebf5fb09fe79
nightcycle <coyer@nightcycle.us>
2026-02-06 18:35:16 -0600

    first draft

---
+++ scripts/build/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+echo "Building project..."
+
+: "${PROJECT:=default.project.json}"
+echo "Using project file: $PROJECT"
+: "${BUILD_PATH:=build.rbxl}"
+echo "Using build path: $BUILD_PATH"
+: "${SOURCEMAP:=sourcemap.json}"
+echo "Using sourcemap path: $SOURCEMAP"
+
+rojo build "$PROJECT" -o "$BUILD_PATH"
+rojo sourcemap "$PROJECT" --output "$SOURCEMAP"
+echo "Build complete."
