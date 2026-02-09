74821a7fe87cc3aeba2a7bf841e65098b9eb8a04
nightcycle <coyer@nightcycle.us>
2026-02-09 03:19:35 -0600

    successfully running entire test suite from shell script

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -1,50 +1,51 @@
 #!/bin/sh
+set -e
 RELEASE_VERSION="${1:?'arg 1, RELEASE_VERSION, is not set'}"
 
-# BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/types/color3
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/types/color3
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/types/rect
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/types/rect
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/types/vector2
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/types/vector2
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/types/vector3
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/types/vector3
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/luau/function
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/luau/function
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/luau/loop
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/luau/loop
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/luau/math
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/luau/math
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# BENCHMARK_PATH=src/server/benchmarks/luau/table
-# export BENCHMARK_PATH
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+BENCHMARK_PATH=src/server/benchmarks/luau/table
+export BENCHMARK_PATH
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
 BENCHMARK_PATH=src/server/benchmarks/luau/tostring
 export BENCHMARK_PATH
 sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-BENCHMARK_PATH=src/server/benchmarks/types/type
+BENCHMARK_PATH=src/server/benchmarks/luau/type
 export BENCHMARK_PATH
 sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-BENCHMARK_PATH=src/server/benchmarks/types/var
+BENCHMARK_PATH=src/server/benchmarks/luau/var
 export BENCHMARK_PATH
 sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
