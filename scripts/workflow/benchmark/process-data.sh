35253af682aa39ad88962fefecb2a5e5df37fef3
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2026-02-08 19:06:20 -0600

    skip raw

--- scripts/workflow/benchmark/process-data.sh
+++ scripts/workflow/benchmark/process-data.sh
@@ -4,20 +4,20 @@ set -e
 RAW_BENCHMARK_RESULT="${1:?'arg 1, RAW_BENCHMARK_RESULT, is not set'}"
 echo "$RAW_BENCHMARK_RESULT" > "$DATA_DIR_PATH/results.json"
 echo "$RAW_BENCHMARK_RESULT"
-RAW_MAP=$(echo "$EXECUTE_RESPONSE" | jq -r '.data.raw')
+# RAW_MAP=$(echo "$EXECUTE_RESPONSE" | jq -r '.data.raw')
 
-# for each key,value in RAW_MAP
-for path in $(echo "$RAW_MAP" | jq -r 'keys[]'); do
-	echo "Processing path: $path"
-	value=$(echo "$RAW_MAP" | jq -r --arg k "$path" '.[$k]')
-	# get directory above the file path
-	dir=$(dirname "${DATA_DIR_PATH}/${path}")
-	# if the directory does not exist
-	if [ ! -d "$dir" ]; then
-		mkdir -p "$dir"
-	fi
-	echo "$value" > "${DATA_DIR_PATH}/${path}"
-done
+# # for each key,value in RAW_MAP
+# for path in $(echo "$RAW_MAP" | jq -r 'keys[]'); do
+# 	echo "Processing path: $path"
+# 	value=$(echo "$RAW_MAP" | jq -r --arg k "$path" '.[$k]')
+# 	# get directory above the file path
+# 	dir=$(dirname "${DATA_DIR_PATH}/${path}")
+# 	# if the directory does not exist
+# 	if [ ! -d "$dir" ]; then
+# 		mkdir -p "$dir"
+# 	fi
+# 	echo "$value" > "${DATA_DIR_PATH}/${path}"
+# done
 
 SUMMARY=$(echo "$EXECUTE_RESPONSE" | jq -r '.data.summary')
 echo "sum $SUMMARY"
