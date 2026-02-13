6ecb544611317010f5c85e383f2960c740ae37f8
nightcycle <coyer@nightcycle.us>
2026-02-12 21:12:19 -0600

    forgot to delete old data

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -2,6 +2,13 @@
 set -e
 RELEASE_VERSION="${1:?'arg 1, RELEASE_VERSION, is not set'}"
 
+# saving the results
+DATA_SUBMODULE_PATH="data"
+cd "$DATA_SUBMODULE_PATH"
+git rm -r src/
+git commit -m "reset data"
+cd ..
+
 # export BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
 # sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
