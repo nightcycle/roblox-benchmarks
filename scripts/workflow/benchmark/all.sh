4bd6f1b1e80aaa581da34c48dd217bd04d3fc70b
nightcycle <coyer@nightcycle.us>
2026-02-14 13:32:54 -0600

    branch is now checked out

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -2,6 +2,11 @@
 set -e
 DATA_RELEASE_VERSION="${1:?'arg 1, DATA_RELEASE_VERSION, is not set'}"
 export DATA_RELEASE_VERSION="$DATA_RELEASE_VERSION"
+
+BRANCH_NAME_ENDING=$(printf '%s' "$DATA_RELEASE_VERSION" | tr '.' '-')
+BRANCH_NAME="release/$BRANCH_NAME_ENDING"
+export BRANCH_NAME="$BRANCH_NAME"
+sh scripts/workflow/benchmark/pull.sh
 sh scripts/workflow/benchmark/reset.sh
 
 # export BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
@@ -9,6 +14,7 @@ sh scripts/workflow/benchmark/reset.sh
 
 # export BENCHMARK_PATH=src/server/benchmarks/types/color3
 # sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+
 
 export BENCHMARK_PATH=src/server/benchmarks/types/rect
 sh scripts/workflow/benchmark/init.sh "$DATA_RELEASE_VERSION"
