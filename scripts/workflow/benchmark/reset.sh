64e761ca61aa3752f570475b114b81acda297622
nightcycle <coyer@nightcycle.us>
2026-02-14 13:38:20 -0600

    no need to reset if it's gone

--- scripts/workflow/benchmark/reset.sh
+++ scripts/workflow/benchmark/reset.sh
@@ -10,6 +10,10 @@ echo "Using data directory: $DATA_DIR_PATH"
 
 cd "$DATA_SUBMODULE_PATH"
 rm -rf "${DATA_DIR_PATH:?}/"
+# if DATA_DIR_PATH doesn't exist, create it
+if [ ! -d "$DATA_DIR_PATH" ]; then
+	exit 0
+fi
 cd ..
 
 sh scripts/workflow/benchmark/commit.sh "resetting ${DATA_SUBMODULE_PATH}"
