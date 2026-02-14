04130401bee6726056339cae9656faf94e9ea68a
nightcycle <coyer@nightcycle.us>
2026-02-14 13:43:34 -0600

    trying more tests

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -5,16 +5,16 @@ export DATA_RELEASE_VERSION="$DATA_RELEASE_VERSION"
 
 BRANCH_NAME_ENDING=$(printf '%s' "$DATA_RELEASE_VERSION" | tr '.' '-')
 BRANCH_NAME="release/$BRANCH_NAME_ENDING"
+
 export BRANCH_NAME="$BRANCH_NAME"
 sh scripts/workflow/benchmark/pull.sh
 sh scripts/workflow/benchmark/reset.sh
 
-# export BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/types/color3
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
-
+export BENCHMARK_PATH=src/server/benchmarks/types/color3
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
 export BENCHMARK_PATH=src/server/benchmarks/types/rect
 sh scripts/workflow/benchmark/init.sh "$DATA_RELEASE_VERSION"
