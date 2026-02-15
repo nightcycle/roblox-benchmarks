49bcc1d9916a397c2a6c1a0bd99eb76ce729514c
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2026-02-14 23:24:13 -0600

    Update all.sh

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -19,29 +19,29 @@ sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 export BENCHMARK_PATH=src/server/benchmarks/types/rect
 sh scripts/workflow/benchmark/init.sh "$DATA_RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/types/vector2
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/types/vector2
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/types/vector3
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/types/vector3
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/function
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/function
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/loop
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/loop
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/math
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/math
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/table
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/table
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/tostring
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/tostring
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/type
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/type
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
 
-# export BENCHMARK_PATH=src/server/benchmarks/luau/var
-# sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+export BENCHMARK_PATH=src/server/benchmarks/luau/var
+sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
