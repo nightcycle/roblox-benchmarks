34749f102fe8700fa1119b99c91386bb2534a715
nightcycle <coyer@nightcycle.us>
2026-02-16 14:20:17 -0600

    added a pull request step

--- scripts/workflow/benchmark/all.sh
+++ scripts/workflow/benchmark/all.sh
@@ -18,49 +18,51 @@ sh scripts/workflow/benchmark/pull.sh
 sh scripts/workflow/benchmark/reset.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/types/brickcolor
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/types/color3
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/types/rect
-sh scripts/workflow/benchmark/init.sh "$DATA_RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/types/vector2
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/types/vector3
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/function
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/loop
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/math/constrain
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/math/noise
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/math/operations
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/math/rounding
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/math/sqrt
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/table
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/tostring
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/type
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
 
 export BENCHMARK_PATH=src/server/benchmarks/luau/var
-sh scripts/workflow/benchmark/init.sh "$RELEASE_VERSION"
+sh scripts/workflow/benchmark/init.sh
+
+gh pr create --base main --head "$BRANCH_NAME" --title "$DATA_RELEASE_VERSION" --fill
